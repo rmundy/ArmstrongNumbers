3c173720ee5dfd6b6da41d8aff14011edf73689c
Bobby Mundy <robert.l.mundy@gmail.com>
2016-01-20 18:11:14 -0500

    Added TextFile

--- ArmstrongNumbers/Program.cs
+++ ArmstrongNumbers/Program.cs
@@ -1,15 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ArmstrongNumbers
+﻿namespace ArmstrongNumbers
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public sealed class Program
     {
-        static void Main(string[] args)
+        public static void Main(String[] args)
         {
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    String line = reader.ReadLine();
+                    if (null == line)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var length = line.Length;
+                        var numberToMatch = Convert.ToInt32(line);
+                        List<Int32> numbers = new List<Int32>();
+                        var total = 0;
+
+                        for (int i = 0; i < length; i++)
+                        {
+                            numbers.Add(Convert.ToInt32(line.Substring(i,1)));
+                            var temp = 1;
+
+                            for (int j = 0; j < length; j++)
+			                {
+			                    temp *= numbers[i];
+			                }
+
+                            total += temp;
+                        }
+
+                        if(numberToMatch == total)
+                        {
+                            Console.WriteLine("True");
+                        }
+                        else
+                        {
+                            Console.WriteLine("False");
+                        }
+                    }
+                }
+            }             
         }
     }
 }
